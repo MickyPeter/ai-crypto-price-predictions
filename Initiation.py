04f2fd3af7a2c43319cf5930f32cbaebf6ce053c
MickyPeter <mihai.peter93@gmail.com>
2025-11-10 20:32:54 +0200

    add Initiation and gitignore

---
+++ Initiation.py
@@ -0,0 +1,9 @@
+pip install seaborn
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+import yfinance as yf
